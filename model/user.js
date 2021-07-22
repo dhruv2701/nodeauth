cdb839d1990d83c74a6c84f3f3fa9e7ffb2386bf
dhruv2701 <sugamgoyal123@gmail.com>
2021-07-22 15:23:37 +0530

    add

--- model/user.js
+++ model/user.js
@@ -39,6 +39,21 @@ const userSchema = new mongoose.Schema({
         }]
 })
 
+userSchema.virtual('tasks', {
+    ref: 'task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
+}
+
 userSchema.statics.findByCredentials = async function(username, password){
     const user = await User.findOne({ username });
         if(!user)
